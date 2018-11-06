521168ab327ce490d167f88a446b942357b856b2
Siyak <siyawandisa@gmail.com>
2018-11-06 16:41:12 +0200

    Utils for settings

---
+++ Utils/Helpers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Configuration;
+
+namespace Utils
+{
+    public static class Helpers
+    {        
+        public static MessagingSettings GetMessagingSettings()
+		{
+			var routingKey = "localhost";
+			var brokerName = "";
+
+			try {
+				routingKey = ConfigurationManager.AppSettings["RoutingKey"];
+                brokerName = ConfigurationManager.AppSettings["BrokerName"];			
+			}
+			catch (Exception ex)
+			{
+                System.Console.WriteLine("error reading configuration file: {0}", ex.Message);                
+			}
+			return new MessagingSettings
+				{
+					RoutingKey = routingKey,
+					BrokerName = brokerName
+				};
+		}
+    }
+}
---
+++ Utils/MessagingSettings.cs
@@ -0,0 +1,5 @@
+public class MessagingSettings
+{
+    public string RoutingKey { get; set; }
+    public string BrokerName { get; set; }
+}
